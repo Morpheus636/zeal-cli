ac04ee277fb8e1512cc96aa9e7e77120255bbba6
Josh Levin <floydaroo@gmail.com>
2022-02-19 18:13:23 -0500

    (#7) Wrote CLI

--- src/zeal_cli.py
+++ src/zeal_cli.py
@@ -1,15 +1,41 @@
-# import argparse
+import argparse
+import shutil
 
-# import zeal
+import zeal
 
 
 def main():
-    pass
-    # parser = argparse.ArgumentParser()
-    # Create args here.
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="action")
 
-    # args = parser.parse_args()
-    # Write the actual logic here.
+    install_command = subparsers.add_parser("install")
+    install_command.add_argument("docsets", nargs="*")
+
+    subparsers.add_parser("list")
+
+    remove_command = subparsers.add_parser("remove")
+    remove_command.add_argument("docsets", nargs="*")
+
+    args = parser.parse_args()
+
+    if args.action == "install":
+        print("Getting list of available docsets")
+        feeds = zeal.downloads.get_feeds()
+        for docset in args.docsets:
+            print(f"Installing docset: {docset}")
+            zeal.docset.download(docset, feeds)
+            print(f"Successfully installed docset: {docset}")
+        print("Cleaning up")
+        shutil.rmtree(feeds)
+        print("Done")
+
+    if args.action == "list":
+        print(*zeal.docset.list_all(), sep="\n")
+    if args.action == "remove":
+        for docset in args.docsets:
+            print(f"Removing docset: {docset}")
+            zeal.docset.remove(docset)
+            print(f"Successfully removed docset: {docset}")
 
 
 if __name__ == "__main__":
