c48e1d37de4829eff0716fec9a51834f57488930
Josh Levin <floydaroo@gmail.com>
2022-02-20 17:31:29 -0500

    (#13) Added parsing for config file.

--- src/zeal/config.py
+++ src/zeal/config.py
@@ -2,11 +2,13 @@ import logging
 import os
 import platform
 
+import yaml
+
 
 logger = logging.getLogger(__file__)
 
 
-def get_docset_dir():
+def get_docset_dir() -> str:
     # Set zeal_docset_dir variable.
     if platform.system() == "Linux":
         return os.path.join(os.path.expanduser("~"), ".local", "share", "Zeal", "Zeal", "docsets")
@@ -14,7 +16,7 @@ def get_docset_dir():
         raise NotImplementedError("Zeal_CLI only supports linux at this time.")
 
 
-def get_cli_data_dir():
+def get_cli_data_dir() -> str:
     if platform.system() == "Linux":
         zeal_cli_dir = os.path.join(os.path.expanduser("~"), ".local", "share", "zeal_cli")
     else:
@@ -36,5 +38,25 @@ def get_cli_data_dir():
     return zeal_cli_dir
 
 
+def set_default_config(config_path: str) -> dict:
+    config_dict = {"docset_dir": get_docset_dir()}
+    with open(config_path, "x") as file:
+        yaml.safe_dump(config_dict, stream=file)
+    return config_dict
+
+
+def get_config(data_dir: str) -> dict:
+    config_path = os.path.join(data_dir, "config.yml")
+    if os.path.isfile(config_path):
+        logger.debug(f"Using config file found at {config_path}.")
+        with open(config_path, "r") as file:
+            config_dict = yaml.safe_load(file)
+    else:
+        logger.warning(f"Did not find a config file at {config_path}. Creating default.")
+        config_dict = set_default_config(config_path)
+    return config_dict
+
+
 cli_data_dir = get_cli_data_dir()
-docset_dir = get_docset_dir()
+config = get_config(cli_data_dir)
+docset_dir = config["docset_dir"]
