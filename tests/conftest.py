3308897bb15616fa20479033d7b9593a97da6969
Dawid Goslawski <dawid@dawidgoslawski.pl>
2022-12-18 19:35:34 +0100

    Ommit winreg, just change a function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 import pytest
 import platform
+from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 @pytest.fixture(scope="session", autouse=True)
 def mock_winreg():
     if platform.system() == "Windows":
-        with patch("src.zeal.config.winreg") as patched_obj:
-            patched_obj.OpenKey.return_value = MagicMock()
-            patched_obj.QueryValueEx.return_value = ("C:/Users/unittestuser/AppData/Local/Zeal/Zeal/docsets", None)
+        with patch("src.zeal.config") as patched_obj:
+            patched_obj.get_docset_dir.return_value = Path("C:/Users/unittestuser/AppData/Local/Zeal/Zeal/docsets")
             yield
     else:
         yield
-
