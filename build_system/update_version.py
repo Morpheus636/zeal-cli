2e3d9aa5c812ced89b25790b5746ea1a2ed3d403
Dawid Goslawski <dawid@dawidgoslawski.pl>
2022-12-18 18:16:50 +0100

    Release job

--- build_system/update_version.py
+++ build_system/update_version.py
@@ -2,14 +2,8 @@ import os
 
 import git
 
-
 repo = git.Repo(search_parent_directories=True)
-try:
-	version = repo.git.describe("--tags")
-except git.exc.GitCommandError:
-    version = "development"
+version = repo.git.describe("--tags")
 
 with open(os.path.join(os.getcwd(), "src", "zeal", "version.py"), "w") as file:
     file.write(f'build_version = "{version}"\n')
-
-#print(f"::set-output name=version::{version}")
