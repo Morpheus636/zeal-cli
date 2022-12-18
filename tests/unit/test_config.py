74111bd8ac82c165b5f0e493d250516914192db3
Dawid Goslawski <dawid@dawidgoslawski.pl>
2022-12-18 19:13:33 +0100

    Skip config test for linux

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -2,9 +2,11 @@ import sys
 import os
 from pathlib import Path
 import pytest
+import platform
 
 sys.path.append(os.path.relpath(__file__) + "/../src")
 import src.zeal  # NOQA: E402
 
+@pytest.mark.skipif(platform.system() != "Windows", reason="Windows only test")
 def test_config_is_mocked_properly():
     assert src.zeal.config.get_docset_dir() == Path("C:/Users/unittestuser/AppData/Local/Zeal/Zealdupa/docsets")
