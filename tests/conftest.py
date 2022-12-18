674fa4e7a991af1c46caae7f3f29b9a616e1a939
Dawid Goslawski <dawid@dawidgoslawski.pl>
2022-12-18 19:07:30 +0100

    Run mock only on Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
+import platform
 from unittest.mock import MagicMock, patch
 
 @pytest.fixture(scope="session", autouse=True)
 def mock_winreg():
-    with patch("src.zeal.config.winreg") as patched_obj:
-        key = MagicMock()
-        patched_obj.OpenKey.return_value = key
-        patched_obj.QueryValueEx.return_value = ("C:/Users/unittestuser/AppData/Local/Zeal/Zealdupa/docsets", None)
+    if platform.system() == "Windows":
+        with patch("src.zeal.config.winreg") as patched_obj:
+            patched_obj.OpenKey.return_value = MagicMock()
+            patched_obj.QueryValueEx.return_value = ("C:/Users/unittestuser/AppData/Local/Zeal/Zealdupa/docsets", None)
+            yield
+    else:
         yield
