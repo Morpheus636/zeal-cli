97e05ff9c4101906d35a99a9149b9d6b99014e46
Dawid Goslawski <dawid@dawidgoslawski.pl>
2022-12-18 16:33:04 +0100

    Temp changes to build_version so I don't have to tag + ifs for upload

--- build_system/update_version.py
+++ build_system/update_version.py
@@ -4,7 +4,10 @@ import git
 
 
 repo = git.Repo(search_parent_directories=True)
-version = repo.git.describe("--tags")
+try:
+	version = repo.git.describe("--tags")
+except git.exc.GitCommandError:
+    version = "development"
 
 with open(os.path.join(os.getcwd(), "src", "zeal", "version.py"), "w") as file:
     file.write(f'build_version = "{version}"\n')
