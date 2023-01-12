edfb16d77050aee298ae6e6a24c797525d6e8eae
Josh Levin <morpheus636@morpheus636.com>
2023-01-12 08:47:27 -0500

    Fixed unit tests for windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 @pytest.fixture(scope="session", autouse=True)
 def mock_winreg():
     if platform.system() == "Windows":
-        with patch("src.zeal.config.config") as patched_obj:
+        with patch("src.zeal_cli.zeal.config.config") as patched_obj:
             patched_obj._get_docset_dir.return_value = Path(
                 "C:/Users/unittestuser/AppData/Local/Zeal/Zeal/docsets"
             )
