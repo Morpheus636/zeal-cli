40862461e23d380b2de502a212b0f7764c19e03a
Josh Levin <morpheus636@morpheus636.com>
2023-01-12 08:41:11 -0500

    updated builds

--- build_system/update_version.py
+++ build_system/update_version.py
@@ -5,5 +5,5 @@ import git
 repo = git.Repo(search_parent_directories=True)
 version = repo.git.describe("--tags")
 
-with open(os.path.join(os.getcwd(), "src", "zeal", "version.py"), "w") as file:
+with open(os.path.join(os.getcwd(), "src", "zeal_cli", "zeal", "version.py"), "w") as file:
     file.write(f'build_version = "{version}"\n')
